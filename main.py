eaf6d4b266eab4ae74de5810d9359e49ec5c456b
Orr Toledano <otoledan@ucsd.edu>
2020-03-31 15:29:08 -0700

    refactor code

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
+from bokeh.core.property.instance import Instance
 from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource, Legend
+from bokeh.models import ColumnDataSource, Legend, CustomJS, LegendItem
 from bokeh.layouts import gridplot
 from bokeh.models.widgets import Dropdown, Select
 from bokeh.plotting import curdoc
@@ -13,16 +14,7 @@ from functools import partial
 from threading import Thread
 import bokeh.server
 
-line_width = 3
-click_policy = "mute"
-muted_alpha = 0.2
-
-time_every = 6
-time_hour = 0 % time_every
-time_minute = 0
-
 def gen_figure_1():
-  global p1, r0, r1, r2, r3, r4, r5
   p1 = figure(x_axis_type="datetime", title="Cases and Deaths as compared to " + county_name + ", " + state_name,
               plot_height=350, plot_width=800)
   p1.xaxis.axis_label = 'Time'
@@ -36,9 +28,9 @@ def gen_figure_1():
   r5 = p1.line('date', 'deaths_state', source=source, color="red", line_dash="dotted", line_width=line_width,
                muted_alpha=muted_alpha)
 
+  return p1, r0, r1, r2, r3, r4, r5
 
 def gen_figure_2():
-  global p2, s0, s1, s2, s3
   p2 = figure(x_axis_type="datetime",
               title="Cases and Deaths per Capita as compared to " + county_name + ", " + state_name, plot_height=350,
               plot_width=800)
@@ -51,9 +43,10 @@ def gen_figure_2():
   s3 = p2.line('date', 'deaths_per_capita', source=source1, color="red", line_dash="dashed", line_width=line_width,
                muted_alpha=muted_alpha)
 
+  return p2, s0, s1, s2, s3
+
 
 def gen_figure_3():
-  global p3, t0, t1, t2, t3
   p3 = figure(x_axis_type="datetime", title="Cases and Deaths over Total Cases and Deaths in State as compared to "
                                             + county_name + ", " + state_name, plot_height=350,
               plot_width=800)
@@ -66,9 +59,9 @@ def gen_figure_3():
   t3 = p3.line('date', 'deaths_per_state', source=source1, color="red", line_dash="dashed", line_width=line_width,
                muted_alpha=muted_alpha)
 
+  return p3, t0, t1, t2, t3
 
 def gen_legend_1():
-  global legend1
   legend1 = Legend(items=[
     ("Cases in State", [r4]),
     ("Deaths in State", [r5]),
@@ -79,9 +72,10 @@ def gen_legend_1():
   ], location=(0, 0))
   legend1.click_policy = click_policy
 
+  return legend1
+
 
 def gen_legend_2():
-  global legend2
   legend2 = Legend(items=[
     ("Cases per Capita", [s0]),
     ("Deaths per Capita", [s1]),
@@ -90,9 +84,10 @@ def gen_legend_2():
   ], location=(0, 0))
   legend2.click_policy = click_policy
 
+  return  legend2
+
 
 def gen_legend_3():
-  global legend3
   legend3 = Legend(items=[
     ("County Cases/State Cases", [t0]),
     ("County Deaths/State Deaths", [t1]),
@@ -101,9 +96,9 @@ def gen_legend_3():
   ], location=(0, 0))
   legend3.click_policy = click_policy
 
+  return legend3
 
-def set_default_state_county_dropdowns():
-  global select_state, select_county, first_state, first_county
+def gen_select_state_county():
   state_list = list(us_counties["state"].unique())
   state_list.sort()
   counties_in_states = list(us_counties.loc[us_counties["state"] == state_list[0]]['county'].unique())
@@ -113,6 +108,9 @@ def set_default_state_county_dropdowns():
 
   select_state.value = state_list[0]
   select_county.value = counties_in_states[0]
+
+  return select_state, select_county
+
 
 
 def when_changing_state(attr, old, new):
@@ -148,54 +146,6 @@ def get_county_dataset(state_name, county_name):
   county = res.get_group(county_name)
 
   return county
-
-
-###############################################################
-
-# Default Comparison County
-state_name = "California"
-county_name = "Los Angeles"
-
-if len(sys.argv) == 3:
-  state_name = sys.argv[1]
-  county_name = sys.argv[2]
-
-if not hasattr(bokeh.server, "get_data"):
-  bokeh.server.get_data = False
-  bokeh.server.data = get_data()
-
-us_counties = bokeh.server.data
-
-first_state = list(us_counties["state"].unique())[0]
-first_county = list(us_counties.loc[us_counties["state"] == first_state]['county'].unique())[0]
-
-source = ColumnDataSource(get_county_dataset(first_state, first_county))
-source1 = ColumnDataSource(get_county_dataset(state_name, county_name))
-
-gen_figure_1()
-gen_figure_2()
-gen_figure_3()
-p = gridplot([[p1], [p2], [p3]])
-
-set_default_state_county_dropdowns()
-
-gen_legend_1()
-gen_legend_2()
-gen_legend_3()
-
-p1.add_layout(legend1, 'right')
-p2.add_layout(legend2, 'right')
-p3.add_layout(legend3, 'right')
-
-select_state.on_change('value', when_changing_state)
-select_county.on_change('value', when_changing_county)
-
-doc = curdoc()
-
-doc.title = "COVID-19 Charts"
-doc.add_root(select_state)
-doc.add_root(select_county)
-doc.add_root(p)
 
 @gen.coroutine
 def update():
@@ -248,6 +198,61 @@ def blocking_task():
     '''
     time.sleep(60)
 
+###############################################################
+
+#Line Settings
+line_width = 3
+click_policy = "mute"
+muted_alpha = 0.2
+
+#Data Update Settings
+time_every = 6
+time_hour = 0 % time_every
+time_minute = 0
+
+
+# Default Comparison County
+state_name = "California"
+county_name = "Los Angeles"
+
+if len(sys.argv) == 3:
+  state_name = sys.argv[1]
+  county_name = sys.argv[2]
+
+if not hasattr(bokeh.server, "get_data"):
+  bokeh.server.get_data = False
+  bokeh.server.data = get_data()
+
+us_counties = bokeh.server.data
+
+first_state = list(us_counties["state"].unique())[0]
+first_county = list(us_counties.loc[us_counties["state"] == first_state]['county'].unique())[0]
+
+source = ColumnDataSource(get_county_dataset(first_state, first_county))
+source1 = ColumnDataSource(get_county_dataset(state_name, county_name))
+
+p1, r0, r1, r2, r3, r4, r5 = gen_figure_1()
+p2, s0, s1, s2, s3 = gen_figure_2()
+p3, t0, t1, t2, t3 = gen_figure_3()
+p = gridplot([[p1], [p2], [p3]])
+
+legend1 = gen_legend_1()
+legend2 = gen_legend_2()
+legend3 = gen_legend_3()
+
+p1.add_layout(legend1, 'right')
+p2.add_layout(legend2, 'right')
+p3.add_layout(legend3, 'right')
+
+select_state, select_county = gen_select_state_county()
+select_state.on_change('value', when_changing_state)
+select_county.on_change('value', when_changing_county)
+
+doc = curdoc()
+doc.title = "COVID-19 Charts"
+doc.add_root(select_state)
+doc.add_root(select_county)
+doc.add_root(p)
 
 thread = Thread(target=blocking_task)
 thread.start()
