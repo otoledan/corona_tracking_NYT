2aea4008ea31d6c3e83fe8c12d53c30db29e24b8
Orr Toledano <otoledan@ucsd.edu>
2020-03-31 01:50:52 -0700

    fix server so that it will only download and process data once, no matter how many sockets there are open

--- main.py
+++ main.py
@@ -11,10 +11,12 @@ from tornado import gen
 import time
 from functools import partial
 from threading import Thread
+import bokeh.server
 
 line_width = 3
 click_policy = "mute"
 muted_alpha = 0.2
+
 
 def gen_figure_1():
   global p1, r0, r1, r2, r3, r4, r5
@@ -25,9 +27,11 @@ def gen_figure_1():
   r0 = p1.line('date', 'cases', source=source, line_width=line_width, muted_alpha=muted_alpha)
   r1 = p1.line('date', 'deaths', source=source, color="red", line_width=line_width, muted_alpha=muted_alpha)
   r2 = p1.line('date', 'cases', source=source1, line_dash="dashed", line_width=line_width, muted_alpha=muted_alpha)
-  r3 = p1.line('date', 'deaths', source=source1, color="red", line_dash="dashed", line_width=line_width, muted_alpha=muted_alpha)
+  r3 = p1.line('date', 'deaths', source=source1, color="red", line_dash="dashed", line_width=line_width,
+               muted_alpha=muted_alpha)
   r4 = p1.line('date', 'cases_state', source=source, line_dash="dotted", line_width=line_width, muted_alpha=muted_alpha)
-  r5 = p1.line('date', 'deaths_state', source=source, color="red", line_dash="dotted", line_width=line_width, muted_alpha=muted_alpha)
+  r5 = p1.line('date', 'deaths_state', source=source, color="red", line_dash="dotted", line_width=line_width,
+               muted_alpha=muted_alpha)
 
 
 def gen_figure_2():
@@ -39,8 +43,10 @@ def gen_figure_2():
   p2.yaxis.axis_label = 'Percent People'
   s0 = p2.line('date', 'cases_per_capita', source=source, line_width=line_width, muted_alpha=muted_alpha)
   s1 = p2.line('date', 'deaths_per_capita', source=source, color="red", line_width=line_width, muted_alpha=muted_alpha)
-  s2 = p2.line('date', 'cases_per_capita', source=source1, line_dash="dashed", line_width=line_width, muted_alpha=muted_alpha)
-  s3 = p2.line('date', 'deaths_per_capita', source=source1, color="red", line_dash="dashed", line_width=line_width, muted_alpha=muted_alpha)
+  s2 = p2.line('date', 'cases_per_capita', source=source1, line_dash="dashed", line_width=line_width,
+               muted_alpha=muted_alpha)
+  s3 = p2.line('date', 'deaths_per_capita', source=source1, color="red", line_dash="dashed", line_width=line_width,
+               muted_alpha=muted_alpha)
 
 
 def gen_figure_3():
@@ -52,8 +58,11 @@ def gen_figure_3():
   p3.yaxis.axis_label = 'Percent People'
   t0 = p3.line('date', 'cases_per_state', source=source, line_width=line_width, muted_alpha=muted_alpha)
   t1 = p3.line('date', 'deaths_per_state', source=source, color="red", line_width=line_width, muted_alpha=muted_alpha)
-  t2 = p3.line('date', 'cases_per_state', source=source1, line_dash="dashed", line_width=line_width, muted_alpha=muted_alpha)
-  t3 = p3.line('date', 'deaths_per_state', source=source1, color="red", line_dash="dashed", line_width=line_width, muted_alpha=muted_alpha)
+  t2 = p3.line('date', 'cases_per_state', source=source1, line_dash="dashed", line_width=line_width,
+               muted_alpha=muted_alpha)
+  t3 = p3.line('date', 'deaths_per_state', source=source1, color="red", line_dash="dashed", line_width=line_width,
+               muted_alpha=muted_alpha)
+
 
 def gen_legend_1():
   global legend1
@@ -99,6 +108,10 @@ def set_default_state_county_dropdowns():
   select_state = Select(title="State:", value="foo", options=state_list)
   select_county = Select(title="County:", value="foo", options=counties_in_states)
 
+  select_state.value = state_list[0]
+  select_county.value = counties_in_states[0]
+
+
 def when_changing_state(attr, old, new):
   state = select_state.value
   counties_in_states = list(us_counties.loc[us_counties["state"] == state]['county'].unique())
@@ -110,16 +123,12 @@ def when_changing_state(attr, old, new):
   state = us_counties.groupby("state").get_group(state_name)
   res = state.groupby("county")
   county = res.get_group(counties_in_states[0])
-
+  select_county.value = counties_in_states[0]
   source.data = county
 
 
 def when_changing_county(attr, old, new):
   state_name = select_state.value
-
-  if state_name == "foo" or state_name == None:
-    state_name = "Alabama"
-
   county_name = select_county.value
 
   state = us_counties.groupby("state").get_group(state_name)
@@ -127,6 +136,7 @@ def when_changing_county(attr, old, new):
   county = res.get_group(county_name)
 
   source.data = county
+
 
 def get_county_dataset(state_name, county_name):
   us_counties["date"] = pd.to_datetime(us_counties['date'])
@@ -136,9 +146,10 @@ def get_county_dataset(state_name, county_name):
 
   return county
 
+
 ###############################################################
 
-#Default Comparison County
+# Default Comparison County
 state_name = "California"
 county_name = "Los Angeles"
 
@@ -179,41 +190,54 @@ doc.add_root(select_state)
 doc.add_root(select_county)
 doc.add_root(p)
 
-@gen.coroutine
+
 def update():
-    temp_source = get_county_dataset(first_state, first_county)
-    temp_source1 = get_county_dataset(state_name, county_name)
+  temp_source = get_county_dataset(first_state, first_county)
+  temp_source1 = get_county_dataset(state_name, county_name)
 
-    source.data = temp_source
-    source1.data = temp_source1
+  source.data = temp_source
+  source1.data = temp_source1
 
-    state_list = list(us_counties["state"].unique())
-    state_list.sort()
-    counties_in_states = list(us_counties.loc[us_counties["state"] == select_state.value]['county'].unique())
-    counties_in_states.sort()
+  state_list = list(us_counties["state"].unique())
+  state_list.sort()
+  counties_in_states = list(us_counties.loc[us_counties["state"] == select_state.value]['county'].unique())
+  counties_in_states.sort()
 
-    select_state.options = state_list
-    select_county.options = counties_in_states
+  select_state.options = state_list
+  select_county.options = counties_in_states
+
+
+if not hasattr(bokeh.server, "get_data"):
+  bokeh.server.get_data = False
+  bokeh.server.data = None
 
 
 def blocking_task():
-    while True:
-      # do some blocking computation
-      cur_time = datetime.now()
+  while True:
+    # do some blocking computation
+    cur_time = datetime.now()
 
-      print("Current Time:", str(cur_time.hour) + ":" + str(cur_time.minute).zfill(2) + ":"
-            + str(cur_time.second).zfill(2))
-      if cur_time.hour % 6 == 0 and cur_time.minute == 00:
+    print("Current Time:", str(cur_time.hour) + ":" + str(cur_time.minute).zfill(2) + ":"
+          + str(cur_time.second).zfill(2))
+    if cur_time.hour % 6 == 0 and cur_time.minute == 0:
+      if bokeh.server.get_data == False:
+        bokeh.server.get_data = True
+
         print("Downloading Data...")
         global us_counties
-        us_counties = get_data(False)
+        bokeh.server.data = get_data(False)
+        us_counties = bokeh.server.data
         print("Downloaded")
 
-        doc.add_next_tick_callback(partial(update))
+    if cur_time.hour % 6 == 0 and cur_time.minute == 5:
+      bokeh.server.get_data = False
+      print("Reset get_data Flag")
+      us_counties = bokeh.server.data
+      doc.add_next_tick_callback(partial(update))
 
-      else:
-        print("Data will be updated at:", str((int(cur_time.hour/6)*6+6)%24) + ":00:00")
-      time.sleep(60)
+    else:
+      print("Data will be updated at:", str((int(cur_time.hour / 6) * 6 + 6) % 24) + ":05:00")
+    time.sleep(60)
 
 
 thread = Thread(target=blocking_task)
